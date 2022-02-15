457f6d072bb0733a493ff6367c0238a666a55b52
Alfred-Sepulveda <79218580+Alfred-Sepulveda@users.noreply.github.com>
2022-02-15 14:18:07 -0800

    Update index.php

--- index.php
+++ index.php
@@ -1,4 +1,8 @@
 <?
 $name = "Alfred Sepulveda";
 echo "Hello ".$name;
+$skills = "Cloud Compute";
+$interest = "Technology";
+echo " These are my skills".$skills;
+echo " and these are my interest".$interest;
 ?>
